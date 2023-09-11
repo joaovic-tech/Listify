2adedcbac67ae80e885bd2f57993a287ba88f480
joaovic <joaovictorca2004@gmail.com>
2023-09-11 15:22:19 -0300

    create route for Erros

--- server/src/routes/user.router.ts
+++ server/src/routes/user.router.ts
@@ -1,12 +1,9 @@
-import "express-async-errors";
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { prisma } from "../utils/prisma";
 import UserController from "../api/User/User.controller";
 import AuthUserController from "../api/auth/AuthUser.Controller";
 import { ensureAuthenticated } from "../middlewares/ensure.authenticated";
 import RefreshTokenUserController from "../api/refreshTokenUser/RefreshTokenUser.Controller";
-import { z } from "zod";
-import { StatusCodes } from "http-status-codes";
 
 const userRoutes = Router();
 
@@ -17,19 +14,6 @@ userRoutes.post("/refresh-token", RefreshTokenUserController.handle);
 userRoutes.get("/users", ensureAuthenticated, async (req, res) => {
   const users = await prisma.user.findMany();
   return res.json(users);
-});
-
-userRoutes.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof z.ZodError) {
-    return res.json({ status: "error", message: error.issues });
-  }
-
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    status: "Error",
-    message: error.message,
-  });
-
-  next();
 });
 
 export { userRoutes };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
+import "express-async-errors";
 import express from "express";
 import { routes } from "./routes";
+import ErrorsController from "./middlewares/Errors.Controller";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", routes);
+app.use("/", ErrorsController.execute);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("HTTP server running!");
